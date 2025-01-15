b82dcb6cbc4fc39fadc849578dca663480c846fd
MeadShalehin <md.meadshalehin@gmail.com>
2025-01-15 14:59:41 +0600

    Initial commit

---
+++ lib/screen/home.dart
@@ -0,0 +1,67 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:rest_api/model/user.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<User> users = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Rest API Call"),
+      ),
+      body: ListView.builder(
+        itemCount: users.length,
+        itemBuilder: (context, index) {
+          final user = users[index];
+          final email = user.email;
+          final color = user.gender == 'male' ? Colors.deepOrange : Colors.pinkAccent;
+
+          return ListTile(
+            title: Text(email, style: TextStyle(color: color)),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: fetchUsers,
+        child: const Icon(Icons.download),
+      ),
+    );
+  }
+
+  void fetchUsers() async {
+    print("Fetch User Called");
+    const url = 'https://randomuser.me/api/?results=20';
+    final uri = Uri.parse(url);
+    final response = await http.get(uri);
+
+    if (response.statusCode == 200) {
+      final body = response.body;
+      final json = jsonDecode(body);
+      final results = json['results'] as List<dynamic>;
+      final transformed = results.map((e) {
+        return User(
+          cell: e['cell'],
+          email: e['email'],
+          gender: e['gender'],
+          nat: e['nat'],
+          phone: e['phone'],
+        );
+      }).toList();
+      setState(() {
+        users = transformed;
+      });
+    }
+
+    print('fetchUsers finished');
+  }
+}
